0c61106647bce63452c84f95dbe3937b1869a361
ahm3tcelik <ahm3tcelik72@gmail.com>
2021-04-01 12:30:36 +0300

    Add meta tags to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,32 @@
 import Link from 'next/link';
+import React from 'react';
+import HeadTag from '../components/common/HeadTag';
+import Meta from '../components/common/Meta';
 import MainLayout from '../components/layouts/MainLayout';
+import { IMeta, IMetaOpenGraph, IMetaPrimary, } from '../types';
+
+const metaPrimary: IMetaPrimary = {
+  title: "Home Page",
+  keywords: "home, Home",
+  author: "ahmet"
+};
+
+const metaOpenGraph: IMetaOpenGraph = {
+  title: "Home",
+  description: "açıklama güzel home index",
+  type: "website"
+}
+
+const metaTags: IMeta = {
+  metaPrimary: metaPrimary,
+  metaOpenGraph: metaOpenGraph
+}
 
 const IndexPage = () => (
   <MainLayout>
+    <HeadTag title="Index Page">
+      <Meta {...metaTags} />
+    </HeadTag>
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
